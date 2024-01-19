0faa155ebd0d44b0c78c6f099015a09a08a08ad7
ifeoluwaoludamilare@gmail.com <ifeoluwaoludamilare@gmail.com>
2024-01-19 06:05:22 -0800

    0-create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creating pupepet file
+
+file {'school':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
